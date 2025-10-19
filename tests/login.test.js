cbfc7bb1b15e985bcd1ffc629e405482698ea7bc
Danielle <daniellesittalopes@gmail.com>
2025-10-19 16:22:29 -0300

    Adicionei fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,12 +1,15 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postlogin.json'));
 
 export const options = {
- stages: [
+ 
+  stages: [
     { duration: '5s', target: 10 },
     { duration: '20s', target: 10 },
     { duration: '5s', target: 0 },
   ],
+  
   thresholds: { 
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01']
@@ -15,11 +18,8 @@ export const options = {
 
 export default function () {
     const url = 'http://localhost:3000/login';
-    
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
+    console.log(postLogin)
+    const payload = JSON.stringify(postLogin);
 
   const params = {
     headers: {
